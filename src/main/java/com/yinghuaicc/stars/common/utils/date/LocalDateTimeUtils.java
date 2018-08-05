01369430264888581f1413a29054d176bf7a0467
xiongjianping <xjp520_1314@163.com>
2018-08-05 18:13:17 +0800

    importExccel

--- src/main/java/com/yinghuaicc/stars/common/utils/date/LocalDateTimeUtils.java
+++ src/main/java/com/yinghuaicc/stars/common/utils/date/LocalDateTimeUtils.java
@@ -133,4 +133,23 @@ public final class LocalDateTimeUtils {
         return maxDate;
     }
 
+
+    /**
+     * Date转换为LocalDateTime
+     * @param date
+     */
+    public static LocalDateTime date2LocalDateTime(Date date){
+        Instant instant = date.toInstant();//An instantaneous point on the time-line.(时间线上的一个瞬时点。)
+        ZoneId zoneId = ZoneId.systemDefault();//A time-zone ID, such as {@code Europe/Paris}.(时区)
+        LocalDateTime localDateTime = instant.atZone(zoneId).toLocalDateTime();
+        return localDateTime;
+      /*  System.out.println(localDateTime.toString());//2018-03-27T14:07:32.668
+        System.out.println(localDateTime.toLocalDate() + " " +localDateTime.toLocalTime());//2018-03-27 14:48:57.453
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");//This class is immutable and thread-safe.@since 1.8
+        System.out.println(dateTimeFormatter.format(localDateTime));//2018-03-27 14:52:57*/
+    }
+
+
+
 }
